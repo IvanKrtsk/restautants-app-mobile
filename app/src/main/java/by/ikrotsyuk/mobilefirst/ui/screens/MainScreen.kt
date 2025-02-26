1cf224d550375cbd078f7e878f52b25352ff9072
IvanKrtsk <ikrotsyuk@gmail.com>
2025-02-26 19:31:43 +0300

    clear main screen

--- app/src/main/java/by/ikrotsyuk/mobilefirst/ui/screens/MainScreen.kt
+++ app/src/main/java/by/ikrotsyuk/mobilefirst/ui/screens/MainScreen.kt
@@ -41,38 +41,17 @@ fun MainScreen() {
             items(restaurantsListState.value){ it ->
                 RestaurantListItem(it)
             }
-            /*item {
-                RestaurantListItem(
-                    RestaurantDTO(
-                        "awda",
-                        "myata",
-                        "zyba",
-                        "kalik",
-                        "https://koko.by/storage/thumbs/place_images/4122/w1600_h1600_q90_aspect_ratio_1_water_016838148561683814856.jpg"
-                    )
-                )
-            }
-            item {
-                RestaurantListItem(
-                    RestaurantDTO(
-                        "awda", "chaynyi pjianitsa",
-                        "internatsionalnaya",
-                        "kalyan",
-                        "https://koko.by/storage/thumbs/place_images/3817/w1600_h1600_q90_aspect_ratio_1_water_216788265231678826523.jpg"
-                    )
-                )
-            }*/
         }
     }
 }
 
 private fun getRestaurants(
     db: FirebaseFirestore,
-    onRestaurants: (List<RestaurantDTO>) -> Unit
+    onGetRestaurants: (List<RestaurantDTO>) -> Unit
 ){
     db.collection("restaurants")
         .get()
         .addOnSuccessListener { task ->
-            onRestaurants(task.toObjects(RestaurantDTO::class.java))
+            onGetRestaurants(task.toObjects(RestaurantDTO::class.java))
         }
 }
